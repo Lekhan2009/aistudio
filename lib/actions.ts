d9ae22db4896e9dac07570fbb39821f5b67622c4
Lekhan2009 <152042199+Lekhan2009@users.noreply.github.com>
2025-06-22 13:23:27 +0530

    Update actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,7 +4,7 @@ import User from "@/models/User";
 import { ProjectForm } from "@/common.types";
 
 const isProduction = process.env.NODE_ENV === 'production';
-const serverUrl = isProduction ? process.env.NEXTAUTH_URL : 'http://localhost:3000';
+const serverUrl = isProduction ? process.env.NEXTAUTH_URL : 'https://aistudio-eta.vercel.app';
 
 export const createProject = async (form: ProjectForm, creatorId: string, token: string) => {
   try {
